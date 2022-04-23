cd05b6789511bec6d5e581f09e771d835c9a04a7
rtambun <richson.tambun@gmail.com>
2022-04-23 11:19:43 +0800

    Add elasticsearch container runner for integration test

--- src/main/java/com/rtambun/scheduler/scheduler/repository/CloseIncidentRepository.java
+++ src/main/java/com/rtambun/scheduler/scheduler/repository/CloseIncidentRepository.java
@@ -2,10 +2,12 @@ package com.rtambun.scheduler.scheduler.repository;
 
 import com.rtambun.scheduler.scheduler.model.CloseIncident;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
 
 import java.time.Instant;
 import java.util.List;
 
+@Repository
 public interface CloseIncidentRepository extends ElasticsearchRepository<CloseIncident, String> {
 
     List<CloseIncident> findByClosedDateAfter (Instant date);
