57ac340af9188e94669f0a82949c1f99e1b38093
rtambun <richson.tambun@gmail.com>
2022-04-23 09:36:34 +0800

    Add model

---
+++ src/test/java/com/rtambun/scheduler/scheduler/SchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rtambun.scheduler.scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
